0d62a240985be50377cc87e2517b1cd5c19bf78e
Jonas Berg <jonas.berg@relex.fi>
2023-12-21 18:27:04 +0200

    Refactor the toplists per language, add ternary plot

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-#Requirements
-Python 3 (version? I'm on 3.9)
-xml.etree.ElementTree (stdlib?)
+#Dutch letters: a final project for the Computational literacy course
+##By Jonas Berg
+
+#Requirements to replicate
+Python 3 (TODO version? I'm on 3.9)
+xml.etree.ElementTree (TODO stdlib?)
 glob (stdlib?)
 matplotlib 3.8.2
+mpltern 1.0.2
 numpy 1.26.2
 
-Older versions of matplotlib and numpy probably work as well
+Older versions of matplotlib, mpltern and numpy probably work as well.
 
-Should work by just running pip install -r requirements.txt from this folder
-or maybe python3 -m pip install -r requirements.txt to ensure you are actually using python3
+The required libraries are hopefully easily just by running `pip install -r requirements.txt` from this folder
+or maybe `python3 -m pip install -r requirements.txt` to ensure you are actually using python3
+
+To get access to the data, download the CKCC data from [their gitlab] or [archived data] and extract it into this folder.
+TODO: check if I can redistribute.
+
+To run the program, just run `python3 main.py` from this folder, most of the output will be to stdout
+and the figures will be in popup windows. You need to close the window to have the program move forward.
--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
+import mpltern
 
 def parse_transcriptions(files):
-    xml_files = glob.glob(files + "/**.xml", recursive=True)
+    xml_files = list()
+    for file in files:
+        xml_files.extend(glob.glob(file + "/**.xml", recursive=True))
     print("Found %d files" % len(xml_files))
     print(xml_files)
     root = ET.Element('root')
@@ -22,7 +25,6 @@ def parse_transcriptions(files):
         # TODO add useful replaced-artifact divs?
         for result in data.iterfind("./text/body/div[@subtype='artifact']"):
             root.append(result)
-    #print(ET.tostring(root.find("./")))
     return root
 
 # For now, just use the available metadata and not the transcription
@@ -43,6 +45,10 @@ def parse_metadata(root_elem):
         author = author_node.get('value')
         date = letter.find("./interp[@type='date']")
         year = year_pattern.match(date.get('value'))
+        # Some letters, especially in Leeuwenhoeck's corpus seem to lack the language element completely or specify it per <div> for summaries, paragraphs etc. Skip for now
+        if letter.find("./interp[@type='language']") is None:
+            print("%s %s" % (author, date.get('value')))
+            continue
         language = letter.find("./interp[@type='language']").get('value')
         if year is not None and author != '?' and language != '?':
             datelist.append(int(year.group()))
@@ -95,6 +101,7 @@ def plot_timeline(metadata, min_date, max_date):
 # Here we want to find the proportions of letters in each language per sender (NB: not necessarily the author)
 def plot_authors(root_elem, authors_list):
     authors = np.array(authors_list)
+    totals = np.zeros(len(authors))
     latin = np.zeros(len(authors))
     french = np.zeros(len(authors))
     dutch = np.zeros(len(authors))
@@ -103,10 +110,13 @@ def plot_authors(root_elem, authors_list):
     italian = np.zeros(len(authors))
     spanish = np.zeros(len(authors))
     greek = np.zeros(len(authors))
+    portuguese = np.zeros(len(authors))
     for i in range(0, len(authors)):
         author = authors[i]
         for letter in metadata:
             if letter['author'] == author:
+                # Hopefully we have already filtered out bad data
+                totals[i] += 1
                 if letter['lang'] == 'la':
                     latin[i] += 1
                 elif letter['lang'] == 'fr':
@@ -123,19 +133,21 @@ def plot_authors(root_elem, authors_list):
                     spanish[i] += 1
                 elif letter['lang'] == 'grc':
                     greek[i] += 1
+                elif letter['lang'] == 'pt':
+                    portuguese[i] += 1
 
     # Look into the minor languages first TODO: discard before later stage?
-    minor_lang_toplist = list(zip(authors, german, english, italian, spanish, greek))
-    languages = ('German', 'English', 'Italian', 'Spanish', 'Ancient Greek')
+    minor_lang_toplist = list(zip(authors, german, english, italian, spanish, portuguese, greek, totals))
+    languages = ('German', 'English', 'Italian', 'Spanish', 'Portuguese', 'Ancient Greek')
     for i, lang in enumerate(languages):
         # Sort in reverse numerical order, only show those who have letters
         sorted_by_lang = sorted(minor_lang_toplist, key=lambda auth: auth[i+1], reverse=True)
         sorted_and_filtered = list(filter(lambda authr: authr[i+1] > 0, sorted_by_lang))
         print()
         print("Authors for %s" % lang)
-        print("%-40s\t %s\t Total letters in minor languages" % ("Name", lang))
+        print("%-40s\t %s\t Total letters" % ("Name", lang))
         for author in sorted_and_filtered:
-            print("%-40s:\t %d\t %d" % (author[0], author[i+1], sum(author[1:])))
+            print("%-40s:\t %d\t %d" % (author[0], author[i+1], author[-1]))
 
     # Could probably use actual numpy methods but haven't yet got the time to do so
     latin_proportion = np.zeros(len(authors))
@@ -150,14 +162,15 @@ def plot_authors(root_elem, authors_list):
 
     # Print a nice table of the results TODO: actually a nice looking one using some library
     # Reserve 40 chars for the names
-    print()
-    print("Major languages only now")
-    print("%-40s\t Latin\t\t French\t\t Dutch\t\t Total" % "Name")
-    for i, author in enumerate(authors):
-        print("%-40s:\t %f\t %f\t %f\t %d" % (author, latin_proportion[i], french_proportion[i], dutch_proportion[i], total_letters[i]))
+    #print()
+    #print("Major languages only now")
+    #print("%-40s\t Latin\t\t French\t\t Dutch\t\t Total" % "Name")
+    #for i, author in enumerate(authors):
+    #    print("%-40s:\t %f\t %f\t %f\t %d" % (author, latin_proportion[i], french_proportion[i], dutch_proportion[i], total_letters[i]))
 
     # Find most prolific authors to make less crowded graphs
     toplist = sorted(zip(authors, latin_proportion, french_proportion, dutch_proportion, total_letters), key=lambda author: author[4], reverse=True)
+    filtered_toplist = list(filter(lambda auth: auth[-1] > 0, toplist))
     print()
     print("Top 100 authors")
     print("%-40s\t Latin\t\t French\t\t Dutch\t\t Total" % "Name")
@@ -166,31 +179,16 @@ def plot_authors(root_elem, authors_list):
 
 
     # For now, look only at senders using more than one language so as to not overwhelm in the plot
-    # Let anyone with nan values slip through for now, they won't be plotted anyway
-    # TODO: make lists of the monolingual authors and the amount of letters they have written
-    multilingual_authors = list()
-    multiling_latin_prop = list()
-    multiling_french_prop = list()
-    multiling_dutch_prop = list()
-    for i, author in enumerate(authors):
-        if latin_proportion[i] != 1.0 and french_proportion[i] != 1.0 and dutch_proportion[i] != 1.0:
-            multilingual_authors.append(author)
-            multiling_latin_prop.append(latin_proportion[i])
-            multiling_french_prop.append(french_proportion[i])
-            multiling_dutch_prop.append(dutch_proportion[i])
+    # TODO: Also limit to senders with more than x letters
+    filtered_only_major_langs = list(filter(lambda auth: auth[1]!= 1.0 and auth[2] != 1.0 and auth[3] != 1.0, filtered_toplist))
+    # Unzip again to make plotting easier
+    multilingual_authors, multiling_latin_prop, multiling_french_prop, multiling_dutch_prop, multiling_totals = zip(*filtered_only_major_langs)
 
     print("Found %d multilingual authors (major languages only)" % len(multilingual_authors))
     # Now the plotting begins
     # TODO: instead of doing a 3d plot, do a ternary scatterplot
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-
-    #xs = latin_proportion
-    #ys = french_proportion
-    #zs = dutch_proportion
-    #ax.scatter(xs, ys, zs)
-    #for i, author in enumerate(authors):
-    #    ax.text(xs[i], ys[i], zs[i], author)
 
     xs = multiling_latin_prop
     ys = multiling_french_prop
@@ -208,8 +206,27 @@ def plot_authors(root_elem, authors_list):
     ax.view_init(elev=60, azim=45, roll=0)
     plt.show()
 
-# Currently only looking at de Groot, expand to the rest later
-letters = parse_transcriptions('ckccRestored/1677705613221-Project_Circulation_of_Kn/original/data/corpus/data/groo001/')
+    # Do a ternary plot
+    fig_tern = plt.figure()
+    ax_tern = fig_tern.add_subplot(projection="ternary")
+
+    ax_tern.set_tlabel("Latin")
+    ax_tern.set_llabel("French")
+    ax_tern.set_rlabel("Dutch")
+
+    ax_tern.grid()
+    ax_tern.scatter(xs, ys, zs)
+    # TODO make the names actually appear in the plot
+    for i, author in enumerate(multilingual_authors):
+        ax_tern.text(xs[i], ys[i], zs[i], author)
+
+    plt.show()
+
+# Currently only looking at de Groot, Christiaan and Constantijn Huyghens, expand to the rest later when I have looked into their xml file layout
+# Leeuwenhoek is also being skipped for now because of a different way of marking language
+# Constantin Huyghens corpus also has the language listed for roughly 100 letters
+folders = ['ckccRestored/1677705613221-Project_Circulation_of_Kn/original/data/corpus/data/groo001/', 'ckccRestored/1677705613221-Project_Circulation_of_Kn/original/data/corpus/data/huyg001/', 'ckccRestored/1677705613221-Project_Circulation_of_Kn/original/data/corpus/data/huyg003/']
+letters = parse_transcriptions(folders)
 metadata, min_date, max_date, authors_list, lang_set = parse_metadata(letters)
 plot_timeline(metadata, min_date, max_date)
 plot_authors(metadata, authors_list)
